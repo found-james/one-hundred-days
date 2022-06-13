bdf1518e7ad5802fbba287e3e3b798731ad67d75
James Suescun <james.steven2318@gmail.com>
2022-06-13 16:06:01 -0400

    complete isUnique

--- 1-isUnique/isUnique.js
+++ 1-isUnique/isUnique.js
@@ -2,4 +2,43 @@
 
 Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
+hints: { 
+    one: use a hash table, 
+    two: try to use a vector, 
+    three: provide solution with big O of nln(n)
+}
+
 */
+
+
+function isUnique (str) {
+
+    if (typeof str !== "string") throw new Error ("strings only")
+
+    let isUnique = true
+    const cache = {}
+    const strArr = str.split("")
+
+
+    for (let char of strArr){
+        if (cache[char]){
+            isUnique = false
+            return isUnique
+        } else {
+            cache[char] = true
+        }
+    }
+
+    return isUnique
+}
+
+
+/* tests */
+
+console.log(isUnique("hello"))
+console.log(isUnique("goodbye"))
+console.log(isUnique("dos"))
+console.log(isUnique(2))
+
+
+
