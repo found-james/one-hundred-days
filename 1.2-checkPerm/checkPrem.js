1c5408067c71b50c64ff9b469ffb7fc29d2e3b73
James Suescun <james.steven2318@gmail.com>
2022-06-13 20:33:23 -0400

    first commit

---
+++ 1.2-checkPerm/checkPrem.js
@@ -0,0 +1,13 @@
+/*
+
+Given two strings, write a method to decide if one is a permutation of the other.
+
+hints: {
+
+    one: be sure you know what a permutation is,
+    two: 3 solution s.t 
+        { timeComplex: nln(n), spaceComplex: n },
+    three: consider a hash table,
+}
+
+*/
