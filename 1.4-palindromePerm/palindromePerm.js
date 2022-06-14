526963eee7faee52333c149a025d3d9901c76ca3
James Suescun <james.steven2318@gmail.com>
2022-06-14 19:27:08 -0400

    first commit

---
+++ 1.4-palindromePerm/palindromePerm.js
@@ -0,0 +1,14 @@
+/*
+
+Given a string, write a function to check if it is a permutation of a palindrome.
+
+Ignore casing and permutations need not be dictionary words.
+
+hints: {
+    one: do not find all the permutations,
+    two: make you sure know what a string permutation is,
+    three: A hash table can provide a result with big O of n - linear time
+    four: use a bit vector?
+}
+*/
+
