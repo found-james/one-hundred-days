70d2e2de9dbc140e528d9b98a2189691802d1cec
James Suescun <james.steven2318@gmail.com>
2022-06-15 19:15:05 -0400

    kinda have a plan. time to code

--- 1.5-oneAway/oneAway.js
+++ 1.5-oneAway/oneAway.js
@@ -1,10 +1,19 @@
 /*
 
 Given two strings, write a function to check if they are at most one edit away.
-Where an edit is insert, remove, or replace
+Where an edit is insert, remove, or replace ONE CHARACTER
 
 hints: {
     one: does it make sense to use the editType as a lens ?
     two: what are the relationships between editTypes?
 }
+*/
+
+/*
+
+editType insert is applicable only if the difference between the string.lengths
+is exactly one
+
+
+
 */
