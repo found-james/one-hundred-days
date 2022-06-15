3319b63b296f5df4e1de1cd1d75e77213c9da141
James Suescun <james.steven2318@gmail.com>
2022-06-15 19:39:16 -0400

    case where strings are same length complete

--- 1.5-oneAway/oneAway.js
+++ 1.5-oneAway/oneAway.js
@@ -9,11 +9,63 @@ hints: {
 }
 */
 
+function oneAway (strA, strB){
+
+    if (typeof strA && typeof strB !== "string" ) throw new Error ("strings only")
+    // case: strings are same length
+
+    if (strA.length === strB.length) {
+        let matches = 0
+        
+        for (let i = 0; i < strA.length; i++){
+            if (strA.charAt(i) === strB.charAt(i)) matches++
+        }
+        
+        return matches === strA.length - 1 || matches === strA.length ? true : false
+    }
+
+
+    // case: strings are not same length
+    let strOne
+    let strTwo
+
+    if (strA.length <= strB.length){
+        strTwo = strA
+        strOne = strB
+    } else {
+        strTwo = strB
+        strOne = strA
+    }
+
+}
+
+console.log(oneAway("hello", "hpllo"))
 /*
 
-editType insert is applicable only if the difference between the string.lengths
-is exactly one
 
+editType insert is applicable only if the difference between the string.lengths
+is exactly one. Same holds true for remove editType.
+
+editType replace is applicable if the string.lenths are equal
+--
+
+Plan
+
+Prompt stipulates that an edit involves an action (insert, remove, replace) of one character
+
+There are two general cases
+
+case one: strings are same length 
+
+-check if strings are equivalent using ===. done
+-if strings 
+
+-check the order of the letters as only one repalce is allowed (swap -- might require two edits)
+*at most two letters can be out of place
+
+
+case two: stringOne.length !== stringTwo.length
+-
 
 
 */
