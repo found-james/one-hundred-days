01c8ef551352d21dae69c508d6dd3b93c586ce60
James Suescun <james.steven2318@gmail.com>
2022-06-15 18:51:43 -0400

    set up prompt

---
+++ 1.5-oneAway/oneAway.js
@@ -0,0 +1,10 @@
+/*
+
+Given two strings, write a function to check if they are at most one edit away.
+Where an edit is insert, remove, or replace
+
+hints: {
+    one: does it make sense to use the editType as a lens ?
+    two: what are the relationships between editTypes?
+}
+*/
