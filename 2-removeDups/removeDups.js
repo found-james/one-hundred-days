0ad612f5ff1b08f8312b4ac69148c5f30bf9000d
James Suescun <james.steven2318@gmail.com>
2022-06-17 18:35:53 -0400

    create classes to model linked list

--- 2-removeDups/removeDups.js
+++ 2-removeDups/removeDups.js
@@ -4,3 +4,28 @@ Write code to remove duplicates from an unsorted linked list
 
 */
 
+class ListNode {
+    constructor(data){
+        this.data = data
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.head = null
+        this.tail = null
+    }
+
+    append(value){
+        let node = new Node(value)
+
+        if (this.head){
+            this.tail.next = node
+            this.tail = node
+        } else {
+            this.head = node
+            this.tail = node
+        }
+    }
+}
