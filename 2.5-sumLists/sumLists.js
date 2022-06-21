44d6b79de872d6ca8b7675dea2070dd77f67e5cd
James Suescun <james.steven2318@gmail.com>
2022-06-21 15:09:27 -0400

    update file structure

---
+++ 2.5-sumLists/sumLists.js
@@ -0,0 +1,11 @@
+/* 
+
+There two numbers represented by linkedLists
+Each node conatins a single digit
+The digits of the number are stored in reverse order (head contains the ones)
+
+Wrtie function that adds the two numbbers and returns the sum as a linkedList
+
+*/
+
+
