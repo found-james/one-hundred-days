87abed4fc2826b00497e8c27bcb3b57450778bc3
James Suescun <james.steven2318@gmail.com>
2022-06-21 15:13:29 -0400

    set up classes to create linkedList data struc

--- 2.5-sumLists/sumLists.js
+++ 2.5-sumLists/sumLists.js
@@ -9,3 +9,28 @@ Wrtie function that adds the two numbbers and returns the sum as a linkedList
 */
 
 
+class listNode {
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class linkedList {
+    constructor (){
+        this.head = null
+        this.tail = null
+    }
+
+    append (node){
+        if (this.head){
+            this.tail.next = node
+            this.tail = node
+        } else {
+            this.head = node
+            this.tail = node
+        }
+    }
+
+}
+
