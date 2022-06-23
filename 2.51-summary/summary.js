3b8ae0206c28e0fe514b52d94f3c701bb973ae01
James Suescun <james.steven2318@gmail.com>
2022-06-22 21:06:38 -0400

    second commit

--- 2.51-summary/summary.js
+++ 2.51-summary/summary.js
@@ -21,4 +21,53 @@ define a class
 constructor: value, next
 
 */
+class listNode {
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+class linkedList {
+    constructor(){
+        this.head = null
+        this.tail = null
+    }
 
+    append(value){
+        let node = new listNode(value)
+
+        if (this.head){
+            this.tail.next = node
+            this.tail = node
+        }
+
+        if(!this.head){
+            this.head = node
+            this.tail = node
+        }   
+
+        return console.log(this)
+    }
+
+    size () {
+        let size = 0
+        let pointer = this.head
+
+        while (pointer){
+            pointer = pointer.next
+            size++
+
+        }
+
+        return size
+    }
+}
+
+let list = new linkedList()
+
+for (let value of ["a", "b", "c", "d", "e"]){
+    list.append(value)
+}
+
+
+console.log(list.size())
