437755c0846aa6f6010c2f50051116acf5025609
James Suescun <james.steven2318@gmail.com>
2022-06-22 20:46:10 -0400

    psuedocode linkedList

--- 2.51-summary/summary.js
+++ 2.51-summary/summary.js
@@ -2,4 +2,23 @@
 
 goal: review linkedlist, stack, queue data structures 
 
+linkedList
+
+define a class
+constructor: head & tail 
+*tail kinda optional*
+
+methods: append, size 
+other methods: prepend, insert, remove, toArray
+
+singly-linked list: one direction only => foward
+doubly-linked list: node <=> node
+
+*linkedLists can be linear or circular*
+
+listNode 
+define a class
+constructor: value, next
+
 */
+
