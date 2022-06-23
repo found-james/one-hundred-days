0a827351333ce040b73b709cdd30e43f2879f0ca
James Suescun <james.steven2318@gmail.com>
2022-06-22 21:10:08 -0400

    refactor spacing

--- 2.51-summary/summary.js
+++ 2.51-summary/summary.js
@@ -2,72 +2,33 @@
 
 goal: review linkedlist, stack, queue data structures 
 
-linkedList
-
-define a class
-constructor: head & tail 
-*tail kinda optional*
-
-methods: append, size 
-other methods: prepend, insert, remove, toArray
-
-singly-linked list: one direction only => foward
-doubly-linked list: node <=> node
-
-*linkedLists can be linear or circular*
-
-listNode 
-define a class
-constructor: value, next
-
 */
 class listNode {
-    constructor(value){
-        this.value = value
-        this.next = null
-    }
+    constructor(value){ this.value = value; this.next = null }
 }
 class linkedList {
-    constructor(){
-        this.head = null
-        this.tail = null
-    }
+    constructor(){ this.head = null; this.tail = null }
 
     append(value){
         let node = new listNode(value)
 
-        if (this.head){
-            this.tail.next = node
-            this.tail = node
-        }
-
-        if(!this.head){
-            this.head = node
-            this.tail = node
-        }   
+        if (this.head){ this.tail.next = node; this.tail = node }
+        if (!this.head){ this.head = node; this.tail = node}   
 
         return console.log(this)
     }
 
     size () {
-        let size = 0
-        let pointer = this.head
+        let size = 0; let pointer = this.head
 
-        while (pointer){
-            pointer = pointer.next
-            size++
-
-        }
-
+        while (pointer){ pointer = pointer.next; size++ }
         return size
     }
 }
 
 let list = new linkedList()
+for (let value of ["a", "b", "c", "d", "e"]) list.append(value)
 
-for (let value of ["a", "b", "c", "d", "e"]){
-    list.append(value)
-}
 
 
 console.log(list.size())
