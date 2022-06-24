f6581739f2bb7f9dfc042ba78a6622fa76a929c5
James Suescun <james.steven2318@gmail.com>
2022-06-23 22:11:18 -0400

    create stack class with push pop and size methods

--- 2.51-summary/summaryII.js
+++ 2.51-summary/summaryII.js
@@ -4,3 +4,21 @@ implement stack data struc using a class + array
 search for alternative method to model stack in JS
 
 */
+
+class Stack {
+    constructor(){
+        this.list = []
+    }
+
+    push (element) { this.list.push(element) }
+
+    pop () { return this.push.pop() }
+
+    size () { return this.list.length }
+
+}
+
+const firstStack = new Stack ()
+for (let elem of ["a", "b", "c"]) firstStack.push(elem)
+
+console.log(firstStack)
