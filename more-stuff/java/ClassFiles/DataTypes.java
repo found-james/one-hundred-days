8795b4ce34a2fb76917c99d773693f18b4d7bacd
James Suescun <james.steven2318@gmail.com>
2022-06-30 21:49:52 -0400

    update class DataTypes

--- more-stuff/java/ClassFiles/DataTypes.java
+++ more-stuff/java/ClassFiles/DataTypes.java
@@ -1,13 +1,16 @@
 package ClassFiles;
-
 class DataTypes {
     
     static byte b = 100;
+    static short s = 2323;
+    static char letter = 'e';
+    static String word = "ghe";
     public static void main (String[] args){
         
-        short s = 2323;
         System.out.println(b + " is type byte");
         System.out.println(s + " is type short");
+        System.out.println(letter + " is type char");
+        System.out.println(word + " is type String");
 
     }
 }
