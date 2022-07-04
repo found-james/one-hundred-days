79d3bd8070500393b637e9775f3e63c36e4464d3
James Suescun <james.steven2318@gmail.com>
2022-07-03 21:37:51 -0400

    create second hashmap

--- stuff/1.2-checkPerm/CheckPerm.java
+++ stuff/1.2-checkPerm/CheckPerm.java
@@ -4,24 +4,28 @@ public class CheckPerm {
 
     public static boolean permutationCheck (String x, String y){
         HashMap<Character, Integer> cacheOne = new HashMap<Character, Integer>();
-        // HashMap<Character, Integer> cacheTwo = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> cacheTwo = new HashMap<Character, Integer>();
 
         for (int i = 0; i < x.length(); i++){
             char letter = x.charAt(i);
-
             if (cacheOne.get(letter) == null){
                 cacheOne.put(letter, 1);
             } else if (cacheOne.get(letter) > 0){
                 int count = cacheOne.get(letter);
                 cacheOne.replace(letter, count, count + 1);
-
                 System.out.println(cacheOne);
             }
-        //         return false;
-        //     } else {
-        //         cacheOne.put(letter, 1);
-        //     }
-        // }
+        }
+
+        for (int i = 0; i < y.length(); i++){
+            char letter = y.charAt(i);
+            if (cacheTwo.get(letter) == null){
+                cacheTwo.put(letter, 1);
+            } else if (cacheTwo.get(letter) > 0){
+                int count = cacheTwo.get(letter);
+                cacheTwo.replace(letter, count, count + 1);
+                System.out.println(cacheTwo);
+            }
         }
         return true;
     }
