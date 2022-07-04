4505d7b30cc660053d442e3511e049076fbd39da
James Suescun <james.steven2318@gmail.com>
2022-07-03 21:01:45 -0400

    set up file struc

---
+++ stuff/1.2-checkPerm/CheckPerm.java
@@ -0,0 +1,15 @@
+public class CheckPerm {
+
+    public static boolean permutationCheck (String x, String y){
+        String i = x;
+        String j = y;
+
+        System.out.println(i);
+        System.out.println(j);
+
+        return true;
+    }
+    public static void main (String[]args){
+        permutationCheck("hi", "bi");
+    }
+}
