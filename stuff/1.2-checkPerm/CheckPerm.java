f9a5c3759a0273c335a608cf65211ffed40c9b5c
James Suescun <james.steven2318@gmail.com>
2022-07-03 21:32:33 -0400

    create freq counter for x

--- stuff/1.2-checkPerm/CheckPerm.java
+++ stuff/1.2-checkPerm/CheckPerm.java
@@ -1,15 +1,31 @@
+import java.util.HashMap;
+
 public class CheckPerm {
 
     public static boolean permutationCheck (String x, String y){
-        String i = x;
-        String j = y;
+        HashMap<Character, Integer> cacheOne = new HashMap<Character, Integer>();
+        // HashMap<Character, Integer> cacheTwo = new HashMap<Character, Integer>();
 
-        System.out.println(i);
-        System.out.println(j);
+        for (int i = 0; i < x.length(); i++){
+            char letter = x.charAt(i);
 
+            if (cacheOne.get(letter) == null){
+                cacheOne.put(letter, 1);
+            } else if (cacheOne.get(letter) > 0){
+                int count = cacheOne.get(letter);
+                cacheOne.replace(letter, count, count + 1);
+
+                System.out.println(cacheOne);
+            }
+        //         return false;
+        //     } else {
+        //         cacheOne.put(letter, 1);
+        //     }
+        // }
+        }
         return true;
     }
     public static void main (String[]args){
-        permutationCheck("hi", "bi");
+        permutationCheck("hiii", "bii");
     }
 }
