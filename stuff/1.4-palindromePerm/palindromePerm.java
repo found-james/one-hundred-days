410dddb29226aeefcc23df1bc8acee900c1f0d27
James Suescun <james.steven2318@gmail.com>
2022-07-06 22:04:03 -0400

    create freq counter and loop over string to cache the chars

--- stuff/1.4-palindromePerm/palindromePerm.java
+++ stuff/1.4-palindromePerm/palindromePerm.java
@@ -1,11 +1,29 @@
+import java.util.HashMap;
+
 public class palindromePerm {
 
     public static boolean checkPerm (String x){
+        HashMap<Character, Integer> freqCounter = new HashMap<Character, Integer>();
 
+
+        for (int j = 0; j < x.length(); j++ ){
+            char key = x.charAt(j); 
+
+            if (freqCounter.get(key) == null){
+                freqCounter.put(key, 1);
+            } else if (freqCounter.get(key) > 0) {
+                int count = freqCounter.get(key);
+                freqCounter.replace(key, count, count + 1);
+            }
+        }
+
+        System.out.println(freqCounter);
         return true;
     }
 
     public static void main (String[]args){
-        
+        String y = new String("okkaay");
+        checkPerm(y);
+
     }
 }
