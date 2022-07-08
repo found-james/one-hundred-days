e449c4a264a05c8c0210a077a317d0428193b251
James Suescun <james.steven2318@gmail.com>
2022-07-08 17:45:41 -0400

    create input bar & useState to store inputt data

--- other-stuff/weather-app/src/App.js
+++ other-stuff/weather-app/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    searchTerm:""
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value});
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // props.movieSearch(formData.searchTerm);
+  };
+
   return (
     <div className="App">
-      Hello
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="searchTerm" onChange={handleChange} value={formData.searchTerm}/>
+        <input type="submit" value="enter zip"/>
+      </form>
     </div>
   );
 }
